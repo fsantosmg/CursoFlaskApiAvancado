382099171f800afe3c42536da3372f1af90dcffa
Felipe Santos <felipe.santos@argosmonitoramento.com.br>
2020-06-30 11:21:57 -0300

    Section 2 schemas modificados para ma.SQLAlchemyAutoSchema.(ModelSchema foi descontinuado). Section 3 configuração para confirmação de cadastro

--- resources/store.py
+++ resources/store.py
@@ -1,13 +1,14 @@
 from flask_restful import Resource
 from models.store import StoreModel
-
-STORE_DELETED = "Store deleted."
-
-ERRO_INSERTING = "An error occurred while creating the store."
+from schemas.store import StoreSchema
 
 NAME_ALREADY_EXISTS = "A store with name '{}' already exists."
-
+ERROR_INSERTING = "An error occurred while inserting the store."
 STORE_NOT_FOUND = "Store not found."
+STORE_DELETED = "Store deleted."
+
+store_schema = StoreSchema()
+store_list_schema = StoreSchema(many=True)
 
 
 class Store(Resource):
@@ -15,35 +16,34 @@ class Store(Resource):
     def get(cls, name: str):
         store = StoreModel.find_by_name(name)
         if store:
-            return store.json()
+            return store_schema.dump(store), 200
+
         return {"message": STORE_NOT_FOUND}, 404
 
     @classmethod
     def post(cls, name: str):
         if StoreModel.find_by_name(name):
-            return (
-                {"message": NAME_ALREADY_EXISTS.format(name)},
-                400,
-            )
+            return {"message": NAME_ALREADY_EXISTS.format(name)}, 400
 
-        store = StoreModel(name)
+        store = StoreModel(name=name)
         try:
             store.save_to_db()
         except:
-            return {"message": ERRO_INSERTING}, 500
+            return {"message": ERROR_INSERTING}, 500
 
-        return store.json(), 201
+        return store_schema.dump(store), 201
 
     @classmethod
     def delete(cls, name: str):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
+            return {"message": STORE_DELETED}, 200
 
-        return {"message": STORE_DELETED}
+        return {"message": STORE_NOT_FOUND}, 404
 
 
 class StoreList(Resource):
     @classmethod
     def get(cls):
-        return {"stores": [x.json() for x in StoreModel.find_all()]}
+        return {"stores": store_list_schema.dump(StoreModel.find_all())}, 200
